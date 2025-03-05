36e3e43661bfebc81504258d303f64e0e54f9247
n1-NISHANT <nk98nishant@gmail.com>
2025-03-05 20:46:28 +0530

    Update ToDoMappingApplication.java

--- Basic/Mapping/OneToManiBi/ToDoMappingApplication.java
+++ Basic/Mapping/OneToManiBi/ToDoMappingApplication.java
@@ -1,1 +1,11 @@
+package SpringBoot.Basic.Mapping.OneToManiBi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoMappingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoMappingApplication.class, args);
+	}
+}
