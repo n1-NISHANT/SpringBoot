4166b92319853a104aa8f4216abc63f1f6d36118
n1-NISHANT <nk98nishant@gmail.com>
2025-03-06 21:10:55 +0530

    Update ServleteInitializer.java

--- Basic/Mapping/OneToManiBi/ServleteInitializer.java
+++ Basic/Mapping/OneToManiBi/ServleteInitializer.java
@@ -1,1 +1,13 @@
+package SpringBoot.Basic.Mapping.OneToManiBi;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ToDoMappingApplication.class);
+	}
+
+}
